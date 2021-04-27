bc84cf669497bb382a3072b23c1a933d2521a5a9
Fozzy <carlmora027@gmail.com>
2021-04-26 23:34:28 -0500

    Retos de la Semana 2 Finalizados

---
+++ Semana 3/Reto-2.js
@@ -0,0 +1,10 @@
+const array = ["Manzana","Pera","Mandarina","Banano","Cereza"];
+
+
+const imprimerElementosArray = () =>{
+    for (let i in array){
+        console.log(`${array}`)
+    };
+};
+
+imprimerElementosArray();
